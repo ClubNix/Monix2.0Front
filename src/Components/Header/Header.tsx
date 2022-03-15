962db1d0be18dfd64e29da9a539ffae9553926bc
Armadindon <baptisteperrin77@gmail.com>
2022-03-15 18:18:18 +0100

    Composant Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => (
+  <div className="header">
+    <img src="logo.png" className="logo" />
+  </div>
+);
+
+export default Header;
