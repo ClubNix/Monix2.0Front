3d98c1901df84e3666f4041f25b63b32fd4d610f
Reis <reisodin@outlook.fr>
2022-03-16 15:35:07 +0100

    Ajout admin Button

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Logo from "../Logo/Logo";
 import "./Header.css";
 
 const Header: React.FC = () => (
   <div className="header">
     <img src="logo.png" className="logo" />
+    <Logo />
   </div>
 );
 
---
+++ src/Components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo: React.FC = () => (
+    <img src="clubnix2019.png" className="nix" onClick={() => alert(`BABABOUM\n \npar Reix
+    \n reconaissance svp   `)}/>
+);
+
+export default Logo;
